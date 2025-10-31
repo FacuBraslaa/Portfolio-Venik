ed90cf9bf8e62a91ef562dbb89c4b10d734c5de9
FacuBraslaa <facundobraslavsky@gmail.com>
2025-10-31 10:11:38 -0300

    .....

--- src/screene/Slide/Slide.jsx
+++ src/screene/Slide/Slide.jsx
@@ -19,27 +19,6 @@ export const Slide = () => {
         src="https://c.animaapp.com/XeGSEg7A/img/line-6.svg"
       />
 
-      import React from "react";
-import "./style.css";
-
-export const Slide = () => {
-  const scrollToSection = (id) => {
-    const element = document.getElementById(id);
-    if (element) {
-      element.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-
-  return (
-    <div className="slide" data-model-id="8:183">
-      <div className="text-wrapper">Portafolio</div>
-
-      <img
-        className="line"
-        alt="Line"
-        src="https://c.animaapp.com/XeGSEg7A/img/line-6.svg"
-      />
-
       <div className="div" onClick={() => scrollToSection("inicio")}>Proyectos</div>
       <div className="text-wrapper-2" onClick={() => scrollToSection("servicios")}>Servicios</div>
       <div className="text-wrapper-3" onClick={() => scrollToSection("sobre-mi")}>Sobre mi</div>
